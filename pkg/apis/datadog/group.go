04d0d69e9d37522a7859f3e332d8ad258fc6e835
Alan Bevier <awbevier@gmail.com>
2020-02-20 19:11:52 -0500

    add api

---
+++ pkg/apis/datadog/group.go
@@ -0,0 +1,6 @@
+// Package datadog contains datadog API versions.
+//
+// This file ensures Go source parsers acknowledge the datadog package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package datadog
---
+++ pkg/apis/datadog/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the datadog v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=datadog.abevier
+package v1alpha1
